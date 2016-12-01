637dfe5057fbf7854861bdc1deacee86baba6a43
Thomas Tuts <thomas@thomastuts.com>
2016-12-01 16:33:00 +0100

    Use Lodash merge() method to avoid removing dependencies

--- src/lib/init.js
+++ src/lib/init.js
@@ -1,6 +1,7 @@
 const exec = require('child_process').execSync;
 const path = require('path');
 const fs = require('fs');
+const merge = require('lodash/merge');
 
 const BEDROCK_REPO = {
   ssh: 'git@github.com:mono-company/bedrock.git'
@@ -31,25 +32,23 @@ module.exports = function (opts) {
   // If no template, copy bedrock repo verbatim to current directory
   exec(`cp -r ${BEDROCK_BASE_DIR}/* .`);
 
-  if (!opts.template) {
-
-  } else {
+  if (opts.template) {
     exec(`git clone ${TEMPLATE_REPOS[opts.template].ssh} ${TEMPLATE_DIR}`);
     exec(`rm -rf content/`);
     exec(`cp -r ${TEMPLATE_DIR}/content .`);
+
+    const bedrockPackageJson = JSON.parse(fs.readFileSync(path.join(BEDROCK_BASE_DIR, 'package.json'), 'utf8'));
+    const templatePackageJson = JSON.parse(fs.readFileSync(path.join(TEMPLATE_DIR, 'package.json'), 'utf8'));
+
+    const generatedPackageJson = merge(bedrockPackageJson, templatePackageJson);
+
+    delete generatedPackageJson.repository;
+    delete generatedPackageJson.bugs;
+    delete generatedPackageJson.homepage;
+
+    fs.writeFileSync('./package.json', JSON.stringify(generatedPackageJson, null, 2));
   }
-
-  const bedrockPackageJson = JSON.parse(fs.readFileSync(path.join(BEDROCK_BASE_DIR, 'package.json'), 'utf8'));
-  const templatePackageJson = JSON.parse(fs.readFileSync(path.join(TEMPLATE_DIR, 'package.json'), 'utf8'));
-
-  const generatedPackageJson = Object.assign({}, bedrockPackageJson, templatePackageJson);
-
-  delete generatedPackageJson.repository;
-  delete generatedPackageJson.bugs;
-  delete generatedPackageJson.homepage;
 
   // Clean up tmp directory
   exec(`rm -rf ${TMP_DIR}`);
-
-  fs.writeFileSync('./package.json', JSON.stringify(generatedPackageJson, null, 2));
 };
